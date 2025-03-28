c9a7765c22b2a3d34522023a0e870944b25c6930
danilo <danilocencigg@gmail.com>
2025-03-28 14:32:31 -0300

    menu programado

--- RoboTupiAP/Program.cs
+++ RoboTupiAP/Program.cs
@@ -2,9 +2,38 @@
 {
     internal class Program
     {
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        public static void MenuInput()
+        {
+            Text.MenuText();
+            string answer = Console.ReadLine();
+            if (answer == "1")
+            {
+
+            } else if (answer == "2")
+            {
+                Text.ExitMsg();
+                Environment.Exit(0);
+            }
+        }
+        
+        
+        
+        
+        
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            MenuInput();
         }
     }
 }
---
+++ RoboTupiAP/Text.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoboTupiAP
+{
+    public class Text
+    {
+        public static void MenuText()
+        {
+            Console.Clear();
+            Console.WriteLine("---------------------");
+            Console.WriteLine("Tupiniquim I");
+            Console.WriteLine("---------------------\n\n");
+            Console.WriteLine("1.Expediçãon\n2.Sair");
+        }
+
+        public static void ExitMsg()
+        {
+            Console.WriteLine("Saindo do Programa...");
+        }
+    
+    
+    }
+}
